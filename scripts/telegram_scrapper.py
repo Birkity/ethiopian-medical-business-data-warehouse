313921a04c0ffbbda8c10a4dc66084f2e091aeb2
Birkity <85987355+Birkity@users.noreply.github.com>
2024-10-10 10:59:14 +0300

    Configure the logger

--- scripts/telegram_scrapper.py
+++ scripts/telegram_scrapper.py
@@ -8,3 +8,11 @@ load_dotenv(r'C:\Users\USER\Documents\OPLearning\10_Academy\Week_7\.env')
 telegram_api_id = os.getenv('TG_API_ID')
 telegram_api_hash = os.getenv('TG_API_HASH')
 user_phone = os.getenv('phone')
+
+logging.basicConfig(
+    filename='channel_scraper.log', 
+    level=logging.INFO, 
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filemode='a'  
+)
+scraper_logger = logging.getLogger()
